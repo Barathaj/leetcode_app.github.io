27a6b9ff04f4d8b6a80822caa9a37d7175d4d654
Barath Raj <130913642+Barathaj@users.noreply.github.com>
2023-08-10 23:46:58 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pickle
 import pandas as pd
-#with open('final_leetcode.pkl','rb') as file:
-   # problems= pickle.load(file)
-problems=pickle.load(open('final_leetcode.pkl','rb'))
+with open('final_leetcode.pkl','rb') as file:
+   problems= pickle.load(file)
+
 st.title("Leetcode recommended problems")
 problems_list=problems['title'].values
 option=st.selectbox(
